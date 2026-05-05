3f6db11c650b11a8e751e6ea9a82cb5a2abfcf36
MaelMAssin <mael.haubourdin@yahoo.com>
2026-05-05 11:49:26 +0200

    app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+from config import USER_INFO
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # C'est ici que la magie opère : on envoie tes infos au HTML
+    return render_template('index.html', info=USER_INFO)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ config.py
@@ -0,0 +1,6 @@
+USER_INFO = {
+    "nom": "Massin",
+    "prenom": "Maël",
+    "titre": "Étudiant en BUT R&T",
+    "email": "mael.haubourdin@yahoo.com"
+}
