49c94137916151c6641cd643598ed1bec3fca80b
MaelMAssin <mael.haubourdin@yahoo.com>
2026-05-07 14:31:43 +0200

    SAE Terminée : Portfolio Full-Stack opérationnel

---
+++ README.md
@@ -0,0 +1,9 @@
+# Portfolio de Maël - SAE23 R&T
+
+Ce projet est un portfolio dynamique géré avec Flask et Docker.
+
+## Installation et Lancement
+1. S'assurer que Docker et Docker Compose sont installés.
+2. Lancer la commande :
+   ```bash
+   sudo docker-compose up --build
--- prog/app.py
+++ prog/app.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 import mimetypes
+import os
 
 mimetypes.add_type('text/css', '.css')
 
 app = Flask(__name__, template_folder='..', static_folder='..', static_url_path='')
 
-# Configuration de la connexion (User:Password@Host/DBname)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://mael:password_mael@db/portfolio_db'
+# On utilise la variable d'environnement fournie par Docker Compose
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'mysql://mael:password_mael@db/portfolio_db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -21,9 +22,13 @@ class Competence(db.Model):
 
 @app.route('/')
 def index():
-    # On récupère toutes les compétences en base de données
-    mes_competences = Competence.query.all()
+    try:
+        mes_competences = Competence.query.all()
+    except:
+        mes_competences = [] # Évite l'erreur si la base n'est pas prête
     return render_template('index.html', competences=mes_competences)
+
+# N'oublie pas d'ajouter les routes ADD et DELETE ici pour la séance 3 !
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
